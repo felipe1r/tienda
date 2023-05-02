34f3330c6e8d17db64fb1c63c820df22cad84bcd
Matias Torres <matia.torresa@duocuc.cl>
2023-05-01 20:00:24 -0400

    a
    a

--- Jscript/vali_inis.js
+++ Jscript/vali_inis.js
@@ -1,5 +1,3 @@
-const nombre = document.getElementById("name")
-const apellido = document.getElementById("lastname")
 const email = document.getElementById("email")
 const pass = document.getElementById("contraseña")
 const form = document.getElementById("form")
@@ -11,16 +9,12 @@ form.addEventListener("submit", e=>{
     let entrar = false
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
     parrafo.innerHTML = ""
-    if(nombre.value.length <6){
-        warnings += 'El nombre no es valido <br>'
-        entrar = true
-    }
     if(!regexEmail.test(email.value)){
-        warnings += 'El email no es valido <br>'
+        warnings += 'El email no es válido <br>'
         entrar = true
     }
     if(pass.value.length < 8){
-        warnings += 'La contraseña no es valida <br>'
+        warnings += 'La contraseña no es válida <br>'
         entrar = true
     }
 
